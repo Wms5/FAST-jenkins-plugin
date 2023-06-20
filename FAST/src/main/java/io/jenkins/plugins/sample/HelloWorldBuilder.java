e9738fdeb1c389602d9c5b79b474cfad2266fee4
Wilkinson Maciel Silva <wilkinson.58.35@gmail.com>
2023-06-19 21:00:53 -0300

    adicao de mais parametros

--- FAST/src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
+++ FAST/src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -20,58 +20,48 @@ import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
-import org.python.util.PythonInterpreter;
 
 public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
-    private final String repositorio;
-    private boolean check;
+
+    private final String name;
+    private boolean useFrench;
 
     @DataBoundConstructor
-    public HelloWorldBuilder(String repositorio) {
-        this.repositorio = repositorio;
-    }
-    public String getRepositorio() {
-        return repositorio;
+    public HelloWorldBuilder(String name) {
+        this.name = name;
     }
 
-    public boolean getCheck(){ return check;}
+    public String getName() {
+        return name;
+    }
+
+    public boolean isUseFrench() {
+        return useFrench;
+    }
 
     @DataBoundSetter
-    public void checkbox(boolean check) {
-        this.check = check;
+    public void setUseFrench(boolean useFrench) {
+        this.useFrench = useFrench;
     }
 
     @Override
-    public void perform(Run<?, ?> run, FilePath workspace, EnvVars env, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
-        listener.getLogger().println("String do repositorio: " + repositorio);
-        listener.getLogger().println("Valor check: " + check);
+    public void perform(Run<?, ?> run, FilePath workspace, EnvVars env, Launcher launcher, TaskListener listener)
+            throws InterruptedException, IOException {
+        listener.getLogger().println("String do repositorio: " + name);
+        listener.getLogger().println("Valor check: " + useFrench);
         try {
             // Caminho para o executável Python
             String pythonExecutable = "python";
-
-            // Caminho para o arquivo Python a ser executado
-            String pythonScript = "C:\\Users\\user\\Desktop\\plugin\\FAST\\src\\main\\resources\\hello.py";
-
-            // Parâmetros do script Python
-            String param1 = "valor1";
-
-            // Comando completo a ser executado
-            String command = pythonExecutable + " " + pythonScript + " " + param1;
-
-            // Criação do processo
+            String pythonScript = "C:\\Users\\user\\Desktop\\TG\\FAST\\src\\main\\resources\\hello.py";
+            String command = pythonExecutable + " " + pythonScript + " " + name;
             ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
             Process process = processBuilder.start();
-
-            // Leitura da saída do processo
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             while ((line = reader.readLine()) != null) {
                 listener.getLogger().println(line);
             }
-
-            // Aguarda a finalização do processo
-            int exitCode = process.waitFor();
-
+            process.waitFor();
         }catch (IOException | InterruptedException e){
             e.printStackTrace();
         }
@@ -82,15 +72,15 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
-        public FormValidation doCheckName(@QueryParameter String value) throws IOException, ServletException {
-            if (value.length() == 0){
-                return FormValidation.error("Insira o parametro 1");
+        public FormValidation doCheckName(@QueryParameter String value, @QueryParameter boolean useFrench)
+                throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error("a");
+            if (value.length() < 4)
+                return FormValidation.warning("b");
+            if (!useFrench && value.matches(".*[éáàç].*")) {
+                return FormValidation.warning("c");
             }
-            /*if (!value.matches("((http|git|ssh|http(s)|file|\\/?)|"
-                    + "(git@[\\w\\.]+))(:(\\/\\/)?)"
-                    + "([\\w\\.@\\:/\\-~]+)(\\.git)(\\/)?")) {
-                return FormValidation.warning("Endereço git errado. Insira novamente");
-            }*/
             return FormValidation.ok();
         }
 
@@ -103,8 +93,5 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         public String getDisplayName() {
             return "FAST";
         }
-
     }
-
-
 }
--- FAST/src/main/resources/hello.py
+++ FAST/src/main/resources/hello.py
@@ -5,8 +5,13 @@ class HelloWorld:
         print("Hello, World!")
 
 param1 = sys.argv[1]
+param2 = sys.argv[2]
+param3 = sys.argv[3]
+param3 = sys.argv[4]
 print("Valor do parametro 1:", param1)
-
+print("Valor do parametro 2:", param2)
+print("Valor do parametro 3:", param3)
+print("Valor do parametro 4:", param4)
 #hello = HelloWorld()
 
 hello.say_hello()
